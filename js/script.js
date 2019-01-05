f281cf8bbb27e18d0fc1b8bbaae26ddc3e1bc381
Aramex <slavbez@mail.ru>
2019-01-06 01:19:39 +0200

    header1

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $(".side-menu").hide();
+   $(".search-barwrap__nav").click(function () {
+       $(".side-menu").slideToggle();
+   });
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
