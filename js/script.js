aa753de459c4dadbf4208dec3242a60028ba55d2
Aramex <slavbez@mail.ru>
2019-01-08 22:22:45 +0200

    header3

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@ $(document).ready(function () {
    $(".search-barwrap__nav").click(function () {
        $(".side-menu").slideToggle();
    });
-
+    $('.slider').bxSlider({
+        pager:false,
+        maxslide:1,
+        minslide:1,
+        moveslide:1,
+        auto:true
+    });
+    $(".bx-wrapper").css("border","none");
 
 });
 
