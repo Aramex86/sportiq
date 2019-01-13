24a5e26d9be7c789fab3e7528f59346f6e1f1315
Aramex <slavbez@mail.ru>
2019-01-13 23:33:36 +0200

    section-items remake

--- js/script.js
+++ js/script.js
@@ -1,17 +1,26 @@
 $(document).ready(function () {
     $(".side-menu").hide();
-   $(".search-barwrap__nav").click(function () {
-       $(".side-menu").slideToggle();
-   });
-    $('.slider').bxSlider({
-        pager:false,
-        maxslide:1,
-        minslide:1,
-        moveslide:1,
-        auto:true,
-        autoHover:true
+    $(".search-barwrap__nav").click(function () {
+        $(".side-menu").slideToggle();
     });
-    $(".bx-wrapper").css("border","none");
+    $('.slider').bxSlider({
+        pager: false,
+        maxslide: 1,
+        minslide: 1,
+        moveslide: 1,
+        auto: true,
+        autoHover: true
+    });
+    $(".bx-wrapper").css("border", "none");
+    $(".section-item__header--item").eq(0).addClass("orange");
+
+    $(".section-item__header--item").click(function () {
+        $(".section-item__header--item").eq(0).removeClass("orange");
+        $(".section-item__header--item").eq(1).removeClass("orange");
+        $(".section-item__header--item").eq(2).removeClass("orange");
+        $(this).toggleClass("orange");
+    });
+
 
 });
 
